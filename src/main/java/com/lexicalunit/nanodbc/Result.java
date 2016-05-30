f4c591c696d8d1dfe451c56f9d3009c6dd5ac57c
Rafee Memon <rafeememon@gmail.com>
2016-05-30 01:12:02 -0700

    Add column value getters

--- src/main/java/com/lexicalunit/nanodbc/Result.java
+++ src/main/java/com/lexicalunit/nanodbc/Result.java
@@ -1,10 +1,38 @@
 package com.lexicalunit.nanodbc;
 
+import java.sql.JDBCType;
+
 public interface Result extends AutoCloseable {
 
-    short columns();
-
     boolean next();
+
+    short getNumColumns();
+
+    String getColumnName(short column);
+
+    JDBCType getColumnType(short column);
+
+    JDBCType getColumnType(String columnName);
+
+    boolean isNull(short column);
+
+    boolean isNull(String columnName);
+
+    int getInt(short column);
+
+    int getInt(String columnName);
+
+    float getFloat(short column);
+
+    float getFloat(String columnName);
+
+    double getDouble(short column);
+
+    double getDouble(String columnName);
+
+    String getString(short column);
+
+    String getString(String columnName);
 
     @Override
     void close();
--- src/main/java/com/lexicalunit/nanodbc/impl/NativeResult.java
+++ src/main/java/com/lexicalunit/nanodbc/impl/NativeResult.java
@@ -1,10 +1,13 @@
 package com.lexicalunit.nanodbc.impl;
 
+import java.sql.JDBCType;
+
 import org.bytedeco.javacpp.Loader;
 import org.bytedeco.javacpp.Pointer;
 import org.bytedeco.javacpp.annotation.Name;
 import org.bytedeco.javacpp.annotation.Namespace;
 import org.bytedeco.javacpp.annotation.Platform;
+import org.bytedeco.javacpp.annotation.StdString;
 
 import com.lexicalunit.nanodbc.Result;
 
@@ -23,9 +26,70 @@ public class NativeResult extends Pointer implements Result {
     private native void allocate();
 
     @Override
-    public native short columns();
+    public native boolean next();
 
     @Override
-    public native boolean next();
+    @Name("columns")
+    public native short getNumColumns();
+
+    @Override
+    @Name("column_name")
+    public native @StdString String getColumnName(short column);
+
+    @Override
+    public JDBCType getColumnType(short column) {
+        return JDBCType.valueOf(getColumnDatatype(column));
+    }
+
+    @Override
+    public JDBCType getColumnType(String columnName) {
+        return JDBCType.valueOf(getColumnDatatype(columnName));
+    }
+
+    @Name("column_datatype")
+    private native int getColumnDatatype(short column);
+
+    @Name("column_datatype")
+    private native int getColumnDatatype(String columnName);
+
+    @Override
+    @Name("is_null")
+    public native boolean isNull(short column);
+
+    @Override
+    @Name("is_null")
+    public native boolean isNull(String columnName);
+
+    @Override
+    @Name("get<int32_t>")
+    public native int getInt(short column);
+
+    @Override
+    @Name("get<int32_t>")
+    public native int getInt(String columnName);
+
+    @Override
+    @Name("get<float>")
+    public native float getFloat(short column);
+
+    @Override
+    @Name("get<float>")
+    public native float getFloat(String columnName);
+
+    @Override
+    @Name("get<double>")
+    public native double getDouble(short column);
+
+    @Override
+    @Name("get<double>")
+    public native double getDouble(String columnName);
+
+    @Override
+    @Name("get<::nanodbc::string_type>")
+    public native @StdString String getString(short column);
+
+    @Override
+    @Name("get<::nanodbc::string_type>")
+    public native @StdString String getString(String columnName);
 
 }
