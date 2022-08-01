c185f84a670c05cdb3ab2bb5470318aa396957e8
Rafee Memon <rafeememon@gmail.com>
2022-07-31 18:56:38 -0700

    Clean up utility classes

--- src/main/java/io/nanodbc/impl/NativeExt.java
+++ src/main/java/io/nanodbc/impl/NativeExt.java
@@ -13,19 +13,24 @@ class NativeExt {
         Loader.load();
     }
 
-    static native void execute(@ByRef NativeResult result, @ByRef NativeConnection conn, String query);
+    public static native void execute(@ByRef NativeResult result, @ByRef NativeConnection conn, String query);
 
-    static native void execute(@ByRef NativeResult result, @ByRef NativeConnection conn, String query, long timeout);
+    public static native void execute(@ByRef NativeResult result, @ByRef NativeConnection conn, String query,
+            long timeout);
 
-    static native void execute(@ByRef NativeResult result, @ByRef NativeStatement statement);
-
-    @Name("just_execute2")
-    static native void justExecute(@ByRef NativeConnection conn, String query);
+    public static native void execute(@ByRef NativeResult result, @ByRef NativeStatement statement);
 
     @Name("just_execute2")
-    static native void justExecute(@ByRef NativeConnection conn, String query, long timeout);
+    public static native void justExecute(@ByRef NativeConnection conn, String query);
+
+    @Name("just_execute2")
+    public static native void justExecute(@ByRef NativeConnection conn, String query, long timeout);
 
     @Name("just_execute")
-    static native void justExecute(@ByRef NativeStatement statement);
+    public static native void justExecute(@ByRef NativeStatement statement);
+
+    private NativeExt() {
+        // utility class
+    }
 
 }
