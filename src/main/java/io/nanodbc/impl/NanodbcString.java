0ee3ce9e2e98bf184e710297f458dc886c07779e
Rafee Memon <rafeememon@gmail.com>
2024-05-09 00:04:58 -0700

    WIP 14

--- src/main/java/io/nanodbc/impl/NanodbcString.java
+++ src/main/java/io/nanodbc/impl/NanodbcString.java
@@ -6,14 +6,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.bytedeco.javacpp.annotation.Adapter;
 import org.bytedeco.javacpp.annotation.Cast;
 
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.PARAMETER})
-@Cast({"std::basic_string", "&"})
-@Adapter("StringAdapter")
+@Cast({"nanodbc::string", "&"})
+// @Adapter("StringAdapter")
 public @interface NanodbcString {
-    String value() default "::nanodbc::string::value_type";
+    // String value() default "::nanodbc::string::value_type";
 }
